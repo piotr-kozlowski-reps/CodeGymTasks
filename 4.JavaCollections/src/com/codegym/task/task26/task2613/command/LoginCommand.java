00f165a41fbbb11d7a04ebef14232e903e430e1e
Piotr Kozlowski <pegaz@ante.pl>
2021-04-09 17:03:30 +0200

    update

--- 4.JavaCollections/src/com/codegym/task/task26/task2613/command/LoginCommand.java
+++ 4.JavaCollections/src/com/codegym/task/task26/task2613/command/LoginCommand.java
@@ -1,23 +1,26 @@
 package com.codegym.task.task26.task2613.command;
 
+import com.codegym.task.task26.task2613.CashMachine;
 import com.codegym.task.task26.task2613.ConsoleHelper;
 import com.codegym.task.task26.task2613.exception.InterruptedOperationException;
 
+import java.util.Enumeration;
+import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginCommand implements Command{
 
     //fileds
-    private String cardNumber = "123456789012";
-    private String pin = "1234";
-
+//    private ResourceBundle validCreditCards = ResourceBundle.getBundle(
+//            CashMachine.class.getPackage().toString().replaceAll("\\.", "\\\\").replace("package ", "")
+//            + "\\resources\\verifiedCards");
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptedOperationException {
 
         while (true) {
-
 
             ConsoleHelper.writeMessage("Write 12-digit cardNumber and 4-digit PIN");
 
@@ -40,21 +43,51 @@ public class LoginCommand implements Command{
                 continue;
             }
 
-            //comparation with hardcoded values
-            if (providedCardNumber.equals(cardNumber) && providedPin.equals(pin)) {
+            //comparation with ResourceBoundle numbers
+            if (validCreditCards.containsKey(providedCardNumber) && providedPin.equals(validCreditCards.getString(providedCardNumber))) {
                 ConsoleHelper.writeMessage("Verification was successful");
                 break;
             } else {
+                ConsoleHelper.writeMessage("Verification failed.");
                 continue;
             }
 
 
+
+
+
+
+
+//            boolean isCardValid = false;
+//            Enumeration<String> keys = validCreditCards.getKeys();
+//
+//            while (keys.hasMoreElements()) {
+//                String key = keys.nextElement();
+//                String value = validCreditCards.getString(key);
+//
+//                if (providedCardNumber.equals(key) && providedPin.equals(value)) {
+//                    isCardValid = true;
+//                }
+//
+//            }
+//
+//            if (isCardValid) {
+//                ConsoleHelper.writeMessage("Verification was successful");
+//                break;
+//            } else {
+//                ConsoleHelper.writeMessage("Verification failed.");
+//                continue;
+//            }
+
         }
-
-
 
     }
 
+//    //todo: wywal
+//    public static void main(String[] args) throws InterruptedOperationException{
+//        LoginCommand loginCommand = new LoginCommand();
+//        loginCommand.execute();
+//    }
+
 
 }
-
